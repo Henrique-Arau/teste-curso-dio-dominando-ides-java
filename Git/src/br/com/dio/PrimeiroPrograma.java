04f015cace4a44c4e4409998e735311e96218447
Henrique-Arau <83648293+Henrique-Arau@users.noreply.github.com>
2021-09-20 23:42:03 -0300

    Add files via upload

---
+++ Git/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		    /*int a = 2;
+		    int b = 3;
+      System.out.println("Hell0 word! " + (a+b));*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npae;
+	
+}
